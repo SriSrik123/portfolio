6c3075fab94d510fad8663c2b02097678776d61e
ss5827 <hellosri2006@gmail.com>
2025-09-07 15:30:18 -0400

    fixed the h

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -37,7 +37,7 @@ export default function ContactSection() {
       icon: MapPin,
       label: "HackerRank",
       value: "hellosri2006",
-      href: "https://www.hackerrank.com/profile/hellosri2006f"
+      href: "https://www.hackerrank.com/profile/hellosri2006"
     }
   ];
 
